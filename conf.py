e4b484401d6b6c8cc62beb9a21433cbb799ce159
keyeswiki <154574239+keyeswiki@users.noreply.github.com>
2025-01-16 15:16:04 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -64,7 +64,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 # 使用 sphinx_rtd_theme 主题  
-html_theme = 'sphinx_rtd_theme'  
+html_theme = 'sphinx_book_theme'  
 
 # 设置 html_sidebars，启用 localtoc 侧边栏  
 html_sidebars = {  
